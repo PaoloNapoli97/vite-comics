74b644787bf4c876cb5a51e8f2c9cb9b9de71d46
PaoloNapoli97 <paolonapoli@live.it>
2022-11-25 16:30:41 +0100

    Add: vite and components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
 
-
+export default {};
 </script>
 
 <template>
+  <div>
+    <AppHeader />
+  </div>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
